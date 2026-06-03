c033d19f027f7e71709d758c2855e65cd3dceb57
XuanzhaoS <t3zhxu00@students.oamk.fi>
2026-06-03 15:22:04 +0300

    build staff dashboard sidebar

---
+++ client/app/admin/page.tsx
@@ -0,0 +1,11 @@
+export default function AdminDashboard() {
+    return (
+      <main className="w-full h-full p-8 flex items-center justify-center">
+        <div className="text-center">
+          <p className="text-slate-500">
+            /admin, Here is the rightside contains chatbox and table status.
+          </p>
+        </div>
+      </main>
+    );
+  }
